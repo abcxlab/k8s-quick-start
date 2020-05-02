6c5a9a0ee9f52ab592d5538045d51f6b77dd9100
王强 <chiangzi@qq.com>
2020-05-02 10:24:08 +0800

    init project

---
+++ shell/pull-kube-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in `kubeadm config images list`; do 
+  imageName=${i#k8s.gcr.io/}
+  docker pull registry.aliyuncs.com/google_containers/$imageName
+  docker tag registry.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
+  docker rmi registry.aliyuncs.com/google_containers/$imageName
+done;
