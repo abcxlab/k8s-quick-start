d2b417976d2a06094903701b39998bd7a16a9674
Wang Qiang <chiangzi@qq.com>
2020-05-04 14:59:05 +0800

    add micro application in springboot

---
+++ app/springboot/micro/src/main/java/com/abcxlab/micro/controller/ServerController.java
@@ -0,0 +1,17 @@
+package com.abcxlab.micro.controller;
+
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+  @RequestMapping("/")
+  public String getHostName() throws UnknownHostException {
+    return InetAddress.getLocalHost().getHostName();
+  }
+
+}
---
+++ app/springboot/micro/src/test/java/com/abcxlab/micro/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcxlab.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
