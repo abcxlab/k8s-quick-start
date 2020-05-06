dac024807c56cc29c44eb89f84605cb728c7e32c
Qiang <chiangzi@qq.com>
2020-05-07 01:45:44 +0800

    add shell scripts for docker/k3s preparation

---
+++ k8s/install-docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#安装系统库依赖
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
+#配置阿里云Docker源
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
+#安装Docker
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+
+
+#当前用户添加docker权限
+sudo usermod -aG docker ${USER}
+
+
+#修改docker配置
+sudo bash -c 'cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ],
+  "registry-mirrors": [
+    "https://hub-mirror.c.163.com"
+  ]
+}
+EOF'
+
+
+#重启docker
+sudo service docker restart
+
+
+#检查配置是否生效
+docker info
---
+++ k8s/install-k8s.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#配置阿里云k8s源
+sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo  apt-key add -
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main
+EOF'
+
+#安装k8s组件
+sudo apt-get -y update
+sudo apt-get install -y kubelet kubeadm kubectl
