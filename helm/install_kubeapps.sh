996a2906786c43ab1e140afb06146d904fa83316
Qiang <chiangzi@qq.com>
2020-06-01 17:15:37 +0800

    update help repo

--- helm/install_kubeapps.sh
+++ helm/install_kubeapps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm update repo 
 kubectl create namespace kubeapps
 helm install kubeapps --namespace kubeapps bitnami/kubeapps --set useHelm3=true
 
